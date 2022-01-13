bc705f8c5c3ee2ff0bc0d1b77561c6bcae2b4f73
mail41 <yhy>
2022-01-13 17:06:05 +0900

    xml 기반 bean 설정

---
+++ src/test/java/ch/fixy/xml/XmlAppContext.java
@@ -0,0 +1,18 @@
+package ch.fixy.xml;
+
+import ch.fixy.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    public void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
